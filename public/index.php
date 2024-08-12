21a1d99e237330485621a8030b85870e73fc0487
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-08-12 15:59:32 -0300

    Update - Base de dados e função para inserir os dados no banco de dados da Resifix

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 date_default_timezone_set("America/Sao_Paulo");
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: DELETE, GET, POST, PUT');
+header('Access-Control-Allow-Methods: DELETE, GET, POST, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json; charset=utf-8');
 
